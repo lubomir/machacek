1d13712e5cb91b274ddae3d4304d42871a346b87
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-11-23 14:24:29 +0100

    Make showTree faster

--- GameTree.hs
+++ GameTree.hs
@@ -97,11 +97,11 @@ getSet new hv set = case M.lookup hv set of
                         Just id' -> (id', set, new)
 
 
-indent :: (a, InformationSets b, Int) -> String -> String
-indent (_,_,n) = (replicate n ' ' ++)
+indent :: (a, InformationSets b, Int) -> [String] -> [String]
+indent (_,_,n) = (replicate n '\t' :)
 
 indentWidth :: Int
-indentWidth = 4
+indentWidth = 1
 
 incIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
 incIndent (x,y,n) = (x,y,n+indentWidth)
@@ -110,16 +110,20 @@ decIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
 decIndent (x,y,n) = (x,y,n-indentWidth)
 
 showTree :: GameTree -> String
-showTree = fst . go (1, M.empty, 0)
+showTree = concat . fst . go (1, M.empty, 0)
   where
-    go acc l@(Leaf _)       = (indent acc (show l++"\n"), acc)
+    go :: (Int, InformationSets Int, Int) -> GameTree -> ([String], (Int, InformationSets Int, Int))
+    go acc l@(Leaf _)       = (indent acc [show l, "\n"], acc)
     go acc (Nature ts)=
-        second decIndent $ foldl walkChildren (indent acc "Nature\n", incIndent acc)
+        second decIndent $ foldl walkChildren (indent acc ["Nature\n"], incIndent acc)
                          $ map snd ts
     go acc@(i,s,ind) (Decide hv p ts)
         = let (id_,newS,newI) = getSet i hv s
-              str = indent acc $ unwords ["Decide",show id_,show p,"\n"]
+              str = indent acc ["Decide ",show id_," ",show p,"\n"]
           in second decIndent $ foldl walkChildren (str, incIndent (newI,newS,ind)) ts
 
+    walkChildren :: ([String], (Int, InformationSets Int, Int))
+                 -> GameTree
+                 -> ([String], (Int, InformationSets Int, Int))
     walkChildren (have, acc) t = let (str, newAcc) = go acc t
                                  in (have ++ str, newAcc)
