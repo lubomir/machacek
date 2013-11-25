7f2c86ddcbd82ea1bcf06b3081ff4398bc8f4cf0
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-11-25 21:15:16 +0100

    Add function for computing the coefficient matrices

--- GameTree.hs
+++ GameTree.hs
@@ -1,6 +1,6 @@
 module GameTree where
 
-import           Data.List  (foldl', nub, sort)
+import           Data.List  (foldl', nub, sort, tails)
 import           Data.Map   (Map)
 import qualified Data.Map   as M
 import           Data.Maybe (fromMaybe, fromJust)
@@ -95,7 +95,7 @@ mkTree k = rollDice [] P1
     call _ _ = error "This can never happen"
 
 data LoopAcc = LA { pool     :: ([Act], [Act])
-                  , assigned :: InformationSets [Act]
+                  , assigned :: InformationSets (Sequence, [Act])
                   , matrix   :: [(Sequence, Sequence, Double)]
                   }
 
@@ -111,19 +111,26 @@ getActsFor n P2 = (\(a,(b,c)) -> (b,(a,c))) . second (splitAt n)
 -- set given by this history view.
 --
 getActs :: Player           -- ^Whose actions we want
+        -> Sequence         -- ^Action sequence used to reach this information set
         -> LoopAcc          -- ^Current loop state
         -> Int              -- ^How many actions we want
         -> HistoryView      -- ^Where to store the actions
         -> ([Act], LoopAcc) -- ^(wanted actions, neww loop state)
-getActs p la n hv = case M.lookup hv (assigned la) of
-                      Nothing   -> let (acts, pool') = getActsFor n p $ pool la
-                                   in (acts, la { assigned = M.insert hv acts (assigned la)
-                                                , pool = pool'
-                                                })
-                      Just acts -> (acts, la)
+getActs p s la n hv =
+    case M.lookup hv (assigned la) of
+        Nothing   -> let (acts, pool') = getActsFor n p $ pool la
+                     in (acts, la { assigned = M.insert hv (s,acts) (assigned la)
+                                  , pool = pool'
+                                  })
+        Just (_,acts) -> (acts, la)
 
 type Sequence = [Act]
 type SeqPair = M.Map Player Sequence
+
+viewBelongsTo :: HistoryView -> Player -> Bool
+(Performed _ : _) `viewBelongsTo` P1 = True
+(Heard _     : _) `viewBelongsTo` P2 = True
+_                 `viewBelongsTo` _  = False
 
 addAct :: Player -> Act -> SeqPair -> SeqPair
 addAct p a = M.insertWith (++) p [a]
@@ -131,7 +138,7 @@ addAct p a = M.insertWith (++) p [a]
 getSequence :: Player -> SeqPair -> Sequence
 getSequence p = fromMaybe [] . M.lookup p
 
-mkActions :: GameTree -> (InformationSets [Act], [(Sequence, Sequence, Double)])
+mkActions :: GameTree -> (InformationSets (Sequence, [Act]), [(Sequence, Sequence, Double)])
 mkActions tree = let res = go 1 M.empty (LA ([1..], [1..]) M.empty []) tree
                  in (assigned res, matrix res)
   where
@@ -146,18 +153,47 @@ mkActions tree = let res = go 1 M.empty (LA ([1..], [1..]) M.empty []) tree
 
     go p sp acc'' (Decide hv pl ts) = foldl' decHelper acc' $ zip acts ts
       where
-        (acts, acc') = getActs pl acc'' (length ts) hv
+        (acts, acc') = getActs pl (getSequence pl sp) acc'' (length ts) hv
         decHelper :: LoopAcc -> (Act, GameTree) -> LoopAcc
         decHelper acc (a,t) = go p (addAct pl a sp) acc t
 
 
+getSequenceMap :: [(Sequence, Sequence, Double)] -> ([(Sequence,Int)],[(Sequence,Int)])
+getSequenceMap ts = let (xs', ys', _) = unzip3 ts
+                        xMap = flip zip [0..] $ sort $ nub $ sort $ concatMap tails $ xs'
+                        yMap = flip zip [0..] $ sort $ nub $ sort $ concatMap tails $ ys'
+                    in (xMap, yMap)
+
+
 mkPayoffMatrix :: [(Sequence, Sequence, Double)] -> Array (Int, Int) Double
-mkPayoffMatrix ts = let (xs', ys', _) = unzip3 ts
-                        xMap = flip zip [(1::Int)..] $ sort $ nub xs'
-                        yMap = flip zip [(1::Int)..] $ sort $ nub ys'
+mkPayoffMatrix ts = let (xMap, yMap) = getSequenceMap ts
                         nullArray = listArray ((0,0),(length xMap,length yMap)) $ repeat 0
                     in foldl' upd nullArray $ map (\(x,y,p) -> ((ml x xMap,ml y yMap), p)) ts
   where
     ml = (fromJust.) . lookup
     upd :: Array (Int,Int) Double -> ((Int,Int),Double) -> Array (Int,Int) Double
     upd arr (i,p) = arr // [(i,p+arr ! i)]
+
+mkConstraintMatrix :: Player                            -- ^Player we are interested in
+                   -> [(Sequence,Int)]                  -- ^This players' actions mapping
+                   -> InformationSets (Sequence, [Act]) -- ^All info sets
+                   -> [[Double]]
+mkConstraintMatrix p m is = (1:replicate (len-1) 0) : map toEq sets
+  where
+    sets = filter ((`viewBelongsTo` p) . fst) $ M.toList is
+    len = 1 + length (nub $ concatMap (snd . snd) sets)
+    toEq (_k,(s,as)) = mkRow (ml s) (map (ml . (:s)) as)
+    ml s = fromJust $ lookup s m
+
+    mkRow :: Int -> [Int] -> [Double]
+    mkRow x vs = reverse (go [] 0 vs)
+      where
+        go acc n (y:ys)
+            | n >= len  = error "This should never happen"
+            | n == x    = go (-1:acc) (n+1) (y:ys)
+            | n == y    = go (1:acc)  (n+1) ys
+            | otherwise = go (0:acc)  (n+1) (y:ys)
+        go acc n []
+            | n >= len  = acc
+            | n == x    = go (-1:acc) (n+1) []
+            | otherwise = go (0:acc)  (n+1) []
