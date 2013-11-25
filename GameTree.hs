3d6ebb6c06d1b4172c912af9fff297d4128ae909
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-11-25 16:01:18 +0100

    Move printing to Main.hs

--- GameTree.hs
+++ GameTree.hs
@@ -1,11 +1,10 @@
 module GameTree where
 
-import Data.List (foldl')
-import Data.Map (Map)
-import Data.Tuple (swap)
-import Data.Maybe
-import qualified Data.Map as M
-import Control.Arrow (second)
+import           Data.List  (foldl')
+import           Data.Map   (Map)
+import qualified Data.Map   as M
+import           Data.Maybe (fromMaybe)
+import           Data.Tuple (swap)
 
 data Player = P1 | P2
     deriving (Eq, Ord, Show)
@@ -133,43 +132,3 @@ mkMatrix tree = let res = go 1 M.empty (LA [1..] M.empty []) tree
         (acts, acc') = getActs acc'' (length ts) hv
         decHelper :: LoopAcc -> (Act, GameTree) -> LoopAcc
         decHelper acc (a,t) = go p (addAct pl a sp) acc t
-
-
-getSet :: Int -> HistoryView -> InformationSets Int -> (Int,InformationSets Int,Int)
-getSet new hv set = case M.lookup hv set of
-                        Nothing  -> (new, M.insert hv new set, new + 1)
-                        Just id' -> (id', set, new)
-
-
-indent :: (a, InformationSets b, Int) -> [String] -> [String]
-indent (_,_,n) = (replicate n '\t' :)
-
-indentWidth :: Int
-indentWidth = 1
-
-incIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
-incIndent (x,y,n) = (x,y,n+indentWidth)
-
-decIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
-decIndent (x,y,n) = (x,y,n-indentWidth)
-
-showTree :: GameTree -> String
-showTree = concat . fst . go (1, M.empty, 0)
-  where
-    go :: (Int, InformationSets Int, Int)
-       -> GameTree
-       -> ([String], (Int, InformationSets Int, Int))
-    go acc l@(Leaf _)  = (indent acc [show l, "\n"], acc)
-    go acc (Nature ts) =
-        second decIndent $ foldl' walkChildren (indent acc ["Nature\n"], incIndent acc)
-                         $ map snd ts
-    go acc@(i,s,ind) (Decide hv p ts)
-        = let (id_,newS,newI) = getSet i hv s
-              str = indent acc ["Decide ",show id_," ",show p,"\n"]
-          in second decIndent $ foldl' walkChildren (str, incIndent (newI,newS,ind)) ts
-
-    walkChildren :: ([String], (Int, InformationSets Int, Int))
-                 -> GameTree
-                 -> ([String], (Int, InformationSets Int, Int))
-    walkChildren (have, acc) t = let (str, newAcc) = go acc t
-                                 in (have ++ str, newAcc)
--- Main.hs
+++ Main.hs
@@ -1,8 +1,49 @@
 module Main where
 
-import GameTree
+import           GameTree
 
-import System.Environment
+import           Control.Arrow      (second)
+import           Data.List          (foldl')
+import qualified Data.Map           as M
+import           System.Environment
+
+getSet :: Int -> HistoryView -> InformationSets Int -> (Int,InformationSets Int,Int)
+getSet new hv set = case M.lookup hv set of
+                        Nothing  -> (new, M.insert hv new set, new + 1)
+                        Just id' -> (id', set, new)
+
+indent :: (a, InformationSets b, Int) -> [String] -> [String]
+indent (_,_,n) = (replicate n '\t' :)
+
+indentWidth :: Int
+indentWidth = 1
+
+incIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
+incIndent (x,y,n) = (x,y,n+indentWidth)
+
+decIndent :: (a,InformationSets b,Int) -> (a,InformationSets b,Int)
+decIndent (x,y,n) = (x,y,n-indentWidth)
+
+showTree :: GameTree -> String
+showTree = concat . fst . go (1, M.empty, 0)
+  where
+    go :: (Int, InformationSets Int, Int)
+       -> GameTree
+       -> ([String], (Int, InformationSets Int, Int))
+    go acc l@(Leaf _)  = (indent acc [show l, "\n"], acc)
+    go acc (Nature ts) =
+        second decIndent $ foldl' walkChildren (indent acc ["Nature\n"], incIndent acc)
+                         $ map snd ts
+    go acc@(i,s,ind) (Decide hv p ts)
+        = let (id_,newS,newI) = getSet i hv s
+              str = indent acc ["Decide ",show id_," ",show p,"\n"]
+          in second decIndent $ foldl' walkChildren (str, incIndent (newI,newS,ind)) ts
+
+    walkChildren :: ([String], (Int, InformationSets Int, Int))
+                 -> GameTree
+                 -> ([String], (Int, InformationSets Int, Int))
+    walkChildren (have, acc) t = let (str, newAcc) = go acc t
+                                 in (have ++ str, newAcc)
 
 main :: IO ()
 main = do
