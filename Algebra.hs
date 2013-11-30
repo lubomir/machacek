a777201264251a49b5620b69ac5a148580497eb2
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-11-30 12:30:16 +0100

    Generalize types

--- Algebra.hs
+++ Algebra.hs
@@ -12,17 +12,15 @@ import qualified Data.Vector        as V
 --
 -- WARNING: while the indices in the third argument start from 0,
 -- Data.Matrix.Matrix is indexed from 1.
-buildMatrix :: Int -> Int -> IntMap (IntMap Double)-> Matrix Double
+buildMatrix :: Num a => Int -> Int -> IntMap (IntMap a) -> Matrix a
 buildMatrix r c m = matrix r c go
   where
     go (x,y) = fromMaybe 0 (I.lookup (x-1) m >>= I.lookup (y-1))
 
--- | Given a matrix and a column vector of variable numbers, produce as column
--- vector produced by matrix multiplication. Tuples are (coefficient, variable).
+-- | Given a matrix and a column vector of variables, create a column vector
+-- produced by matrix multiplication. Tuples are (coefficient, variable).
 --
-matMult :: Matrix Double -> [Int] -> [[(Double, Int)]]
+matMult :: Matrix a -> [b] -> [[(a, b)]]
 matMult m vs = map go [1..nrows m]
   where
-    go :: Int -> [(Double, Int)]
-    go i = let row = i `getRow` m
-           in zip (V.toList row) vs
+    go i = zip (V.toList $ i `getRow` m) vs
