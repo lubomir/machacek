1ca82b4b909fa90f81af4aad94f54b0686a0e5a9
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-12-07 13:09:05 +0100

    Actually find strategy for first player

--- Main.hs
+++ Main.hs
@@ -8,6 +8,10 @@ import           Data.IntMap           (IntMap)
 import qualified Data.IntMap           as I
 import           Data.List             (partition)
 import           Data.List             (intercalate)
+import qualified Data.Map              as M
+import           Data.Maybe (fromJust)
+import qualified Data.ListTrie.Patricia.Map     as T
+import           Data.ListTrie.Patricia.Map.Ord (TrieMap)
 import           Numeric.LinearAlgebra (fromBlocks, fromLists, rows, trans)
 import           System.Environment
 import           Text.Printf
@@ -31,8 +35,8 @@ makeLP k = do
         payoffMatrix = mkPayoffMatrix seqs
         matE = fromLists $ mkConstraintMatrix P1 xMap acts
         matF = fromLists $ mkConstraintMatrix P2 yMap acts
-        xs = map (('x':) . show) [1..rows payoffMatrix]
-        zs = map (('z':) . show) [1..rows matF]
+        xs = map (('x':) . show) [0..rows payoffMatrix-1]
+        zs = map (('z':) . show) [0..rows matF-1]
 
     maximize $ head zs
     constrain "=" (matMult matE xs) (1:repeat 0)
@@ -58,12 +62,38 @@ parseVars = (toMap *** toMap) . partition ((=='x') . head) . filter hasVar . lin
     toMap :: [String] -> IntMap Double
     toMap = I.fromList . map (toPair . words)
 
+getStrategy :: TrieMap Act Int
+            -> IntMap Double
+            -> [(HistoryView, (Sequence, [Act]))]
+            -> IO ()
+getStrategy m vars sets = mapM_ toDecision sets
+  where
+    ml = fromJust . flip T.lookup m
+    toDecision (hist, (sq, actions)) = if parent > 0
+        then do
+            putStrLn $ "Situation " ++ show hist
+            mapM_ go $ zip [0..] $ map (/parent) as
+        else return ()
+      where
+        parent = fromJust $ I.lookup (ml sq) vars
+        as = map (fromJust . flip I.lookup vars .  ml . (\x -> x : sq)) actions
+
+        go :: (Int, Double) -> IO ()
+        go (_,0) = return ()
+        go (n,p) = putStrLn $ printf "  %s with prob. %.3f" a p
+          where
+            a = case last hist of
+                    Heard _     -> if n == 0 then "Trust" else "Do not trust"
+                    Performed e -> "Say " ++ show (rolled e + n)
+
 makeStrategy :: Int -> IO ()
 makeStrategy k = do
     let (acts, seqs) = mkActions $ mkTree k
+        (xMap, _yMap) = getSequenceMap seqs
     inp <- getContents
     let vars = parseVars inp
-    print vars
+    let actsForP1 = filter ((`viewBelongsTo` P1) . fst) $ M.toList acts
+    getStrategy xMap (fst vars) actsForP1
 
 main :: IO ()
 main = do
